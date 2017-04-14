073b0d87aabadabfca059a30823a955bb7db9be0
ranranup <1396522879@qq.com>
2017-04-14 09:46:40 +0800

    20170414

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,6 @@ function init() {
 function start() {
     var startUrl = $("#startUrl").val();
     var total = $("#total").val();
-    alert("提交成功！");
     $.ajax({
         url: "/api/start",
         type: "POST",
@@ -83,13 +82,85 @@ function showDetail(tasksId) {
     layui.use('layer', function() {
         layer.config({
             title: "Task详情",
-            area: ['50%', '40%']
+            area: ["625px","520px"]
         });
         layer.open({
             type: 1,
             content: $('#panel-detail') //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
         });
     });
+    $.ajax({
+        url: "/api/getTask",
+        type: "POST",
+        dataType: "json",
+        data: {
+            "taskId": tasksId
+        },
+        success: function(data) {
+            var task = data.data.task;
+            var status = "";
+            var pageRankTime = "";
+
+            if (task.status == 0) {
+                status = "新创建";
+            } else if (task.status == 1) {
+                status = "爬取中......";
+            } else if (task.status == 2) {
+                status = "爬取结束";
+            } else if (task.status == 3) {
+                status = "计算中......";
+                pageRankTime = "尚未计算结束";
+            } else {
+                status = "计算完成";
+            }
+
+            $('#panel-detail .layui-table').empty();
+            
+            $('#panel-detail .layui-table').append(`<tr>
+                          <th>id</th>
+                          <td>${task.id}</td>
+                      </tr>
+                      <tr>
+                          <th>开始网址</th>
+                          <td>${task.startUrl}</td>
+                      </tr>
+                      <tr>
+                          <th>已处理数</th>
+                          <td>${task.hasHandled}</td>
+                      </tr>
+                      <tr>
+                          <th>目标总数</th>
+                          <td>${task.totalUrl}</td>
+                      </tr>
+                      <tr>
+                          <th>状态</th>
+                          <td>${status}</td>
+                      </tr>
+                      <tr>
+                          <th>创建时间</th>
+                          <td>${task.createTime}</td>
+                      </tr>
+                      <tr>
+                          <th>爬取开始时间</th>
+                          <td>${task.crawlStartTime}</td>
+                      </tr>
+                      <tr>
+                          <th>爬取结束时间</th>
+                          <td>${task.crawlEndTime}</td>
+                      </tr>
+                      <tr>
+                          <th>导出时间</th>
+                          <td>${task.exportTime}</td>
+                      </tr>
+                      <tr>
+                          <th>计算时间</th>
+                          <td>${pageRankTime}</td>
+                      </tr>`);
+        },
+        error: function() {
+
+        }
+    });    
 };
 
 function deleteTask(taskId) {
@@ -97,7 +168,7 @@ function deleteTask(taskId) {
         layer.confirm('删除后无法恢复，确认删除?', {
             icon: 3,
             title: '友情提示',
-            area: ""
+            area: ''
         }, function(index) {
             $.ajax({
                 url: "/api/deleteTask",
